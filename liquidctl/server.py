6bd795176e0b808ae0d3559aad7baab48f7badc7
Thibault Duplessis <t@lichess.org>
2020-11-23 21:54:22 +0100

    refine and add manual mode

--- liquidctl/server.py
+++ liquidctl/server.py
@@ -31,14 +31,14 @@ class CromStatus:
     cpu_temp: int
 
 PROFILE = [
-    # water aio     cpu     rear    top     RGB
-    # temp  pump%   duty%   duty%   duty%   theme
-    [0,     30,     0,      0,      0,      "frost"],
-    [25,    45,     30,     0,      0,      "cool"],
-    [26,    60,     50,     40,     0,      "tepid"],
-    [27,    75,     70,     40,     40,     "warm"],
-    [28,    90,     100,    60,     60,     "toasty"],
-    [29,    100,    100,    100,    100,    "fusion"]
+    # water     aio     cpu     rear    top     RGB
+    # tempº     pump%   duty%   duty%   duty%   theme
+    [0,         30,     0,      0,      0,      "frost"],
+    [26,        45,     30,     0,      0,      "cool"],
+    [27,        60,     50,     40,     0,      "tepid"],
+    [28,        75,     70,     40,     40,     "warm"],
+    [29,        90,     100,    60,     60,     "toasty"],
+    [30,        100,    100,    100,    100,    "fusion"]
 ]
 
 class Server:
@@ -63,8 +63,11 @@ class Server:
 
             mode = self.mode_from_water_temp(status.cooling.aio.water_temp)
 
+            manual = self.read_manual_mode()
+            mode = manual if manual and mode - manual < 4 else mode
+
             if mode != self.mode:
-                self.journal(f'Mode: {self.mode} -> {mode}')
+                self.journal(f'Mode: {self.mode} -> {mode} ({PROFILE[mode]})')
                 self.mode = mode
                 self.rgb.set_mode(mode)
                 self.cooling.set_mode(mode)
@@ -97,6 +100,15 @@ class Server:
             self.journal(e)
             return 99 # assume the worst
 
+    def read_manual_mode(self):
+        try:
+            mode = max(0, min(len(PROFILE) - 1, int(open("/tmp/crom-mode").read())))
+            if mode != self.mode:
+                self.journal(f'Manual mode: {mode}')
+            return mode
+        except:
+            return None
+
 class Cooling:
 
     def __init__(self, aio, case):
@@ -106,12 +118,15 @@ class Cooling:
     def status(self):
         ks = self.aio.get_status()
         water_temp = float(ks[0][1])
+        if not water_temp or water_temp < 15:
+            print(f'ERROR bad water temperature: {water_temp}')
+            water_temp = 28
         cs = self.case.get_status()
         def fan_value(fan_id: int, name: str):
             return int(next((f[1] for f in cs if f[0] == f'Fan {fan_id} {name}'), 0))
         return CoolingStatus(
             AioStatus(
-                water_temp if water_temp and water_temp > 15 else 99, # conservative failsafe
+                water_temp,
                 CoolerStatus(int(ks[1][1]), int(ks[2][1]))
             ),
             CaseStatus(
@@ -138,36 +153,41 @@ class Rgb:
         self.set_theme(PROFILE[mode][5])
 
     def set_theme(self, theme: str):
+        self.strip("fading", "ffffff 0000ff 00ff00 ff0000")
         if theme == "frost":
             self.ring("fading", "000033 0011ff 0000ff")
             self.logo("fading", "000066 000033")
-        if theme == "cool":
+            self.strip("fixed", "0000ff")
+        elif theme == "cool":
             self.ring("fading", "330000 ff1100 ff0000")
             self.logo("fading", "660000 330000")
         elif theme == "tepid":
             self.ring("fading", "330000 ff1100 ff0000", "fastest")
             self.logo("fading", "660000 330000", "faster")
-        elif theme == "warm" or theme == "toasty":
-            self.ring("tai-chi", "ff0000 ff1a00", "fastest")
+        elif theme == "warm":
+            self.ring("tai-chi", "ff0000 ff2a00", "fastest")
             self.logo("fading", "880000 881100", "fastest")
+        elif theme == "toasty":
+            self.ring("tai-chi", "ff0000 ff5500", "fastest")
+            self.logo("fading", "aa0000 aa3300", "fastest")
+            self.strip("fading", "ff0000 0000ff", "fastest")
         elif theme == "fusion":
             self.ring("wings", "0000ff")
-            self.logo("fading", "0000ff 666666")
-        elif theme == "error":
-            self.ring("wings", "ffffff", "faster")
-            self.logo("fading", "ff0000 666666", "fastest")
-        elif theme == "change":
-            self.ring("spectrum-wave", None, "faster")
-            self.logo("fixed", "555555")
-        else:
+            self.logo("spectrum-wave", None, "fastest")
+            self.strip("spectrum-wave", None, "fastest")
+        else: # error feedback
             self.ring("fixed", "000000")
             self.logo("fading", "660000 000000", "fastest")
+            self.strip("alternating-3", "ffffff ff0000", "fastest")
 
     def ring(self, mode, colors, speed = "normal"):
         self.aio.set_color("ring", mode, self.color_map(colors), speed=speed)
 
     def logo(self, mode, colors, speed = "normal"):
         self.aio.set_color("logo", mode, self.color_map(colors), speed=speed)
+
+    def strip(self, mode, colors = None, speed = "normal"):
+        self.case.set_color("led1", mode, self.color_map(colors), speed=speed)
 
     def color_map(self, colors: str):
         return map(color_from_str, colors.split(' ') if colors else [])
